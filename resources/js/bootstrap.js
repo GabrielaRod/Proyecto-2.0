6dd3baaf4b9ae01433a25d1e43b53910c508947a
Gabriela <gabrielarodb@gmail.com>
2021-08-31 19:48:07 -0400

    Nada

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -22,7 +22,7 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
      broadcaster: 'pusher',
-     key: process.env.MIX_PUSHER_APP_KEY,
-     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+     key: 'ecce705f215b11ddad4e',
+     cluster: 'mt1',
      forceTLS: true
 });
--- resources/views/livefeed/index.blade.php
+++ resources/views/livefeed/index.blade.php
@@ -5,10 +5,7 @@
         </h2>
     </x-slot>
     <div>
-        <div id="app">
-            <example-component></example-component>
-        </div>
-        {{-- <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             <div class="flex flex-col">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -47,7 +44,6 @@
                     </div>
                 </div>
             </div>
-
-        </div> --}}
+        </div>
     </div>
 </x-app-layout>

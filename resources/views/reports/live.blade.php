fb1469081f56ea02d321d0ff0a0d20eaa2ad858c
Gabriela <gabrielarodb@gmail.com>
2021-12-02 17:48:03 -0500

    Minor Changes
    Last change.

--- resources/views/reports/live.blade.php
+++ resources/views/reports/live.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
-            Alertas
+            Transmisión
         </h2>
     </x-slot>
     <div class="py-12 px-6 py-3 bg-gray-50 text-center text-xs font-light text-gray-500 tracking-wider">
